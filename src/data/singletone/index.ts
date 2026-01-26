856e8ab528a42068e7cd04dd14f7faa2fa80228c
zelld <zeLLa444@gmail.com>
2026-01-27 00:23:51 +0700

    add comment

--- src/data/singletone/index.ts
+++ src/data/singletone/index.ts
@@ -1,9 +1,11 @@
 import Database from "@/data/db";
 import {MovieRepository} from "@/data/repository/movies.ts";
 
+//Одиночка — это порождающий паттерн проектирования, который гарантирует, что у класса есть только один экземпляр, и предоставляет к нему глобальную точку доступа.
+
 const database = new Database()
 
- 
+
 const movieRepository = new MovieRepository(database)
 
 
