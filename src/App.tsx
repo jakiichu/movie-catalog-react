02bc9ecea7862613a0c15bc37986f6626f875ee4
zelld <zeLLa444@gmail.com>
2026-01-27 02:28:46 +0700

    add query config

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import type {ReactNode} from 'react';
 import {RouterProvider} from "react-router";
 import router from "@/app/router";
 import './index.scss'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient()
+
 const App = (): ReactNode => {
     return (
-        <RouterProvider router={router}/>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools/>
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
     );
 };
 
---
+++ src/app/common/enum/query.ts
@@ -0,0 +1,6 @@
+const QUERY_KEY = {
+    GET_ALL_MOVIE: 'GET_ALL_MOVIE'
+} as const
+
+
+export {QUERY_KEY}
