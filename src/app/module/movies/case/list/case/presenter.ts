0b042b4bd2d96ddeaa19875b4886daaa571fbd3e
zelld <zeLLa444@gmail.com>
2026-01-28 12:09:35 +0700

    use debounce in get all movies

--- src/app/module/movies/case/list/case/presenter.ts
+++ src/app/module/movies/case/list/case/presenter.ts
@@ -3,25 +3,30 @@ import {useGetAllMoviesRequest} from "@/app/module/movies/case/list/case/request
 import {useForm} from "react-hook-form";
 import {useEffect} from "react";
 import {MoviesFiltersManager} from "@/app/utils/filters-manager.ts";
+import {useDebounce} from "@/app/utils/use-debounce.ts";
 
 const moviesFiltersManager = new MoviesFiltersManager();
 
 const useGetAllMoviesPresenter = () => {
-
-    const defaultValues = moviesFiltersManager.getAll()
+    const defaultValues = moviesFiltersManager.getAll();
 
     const form = useForm<IGetAllMoviesForm>({
-        defaultValues
-    })
+        defaultValues,
+    });
 
+    const watchedValues = form.watch();
+
+    const debouncedValues = useDebounce(watchedValues, 500);
 
     useEffect(() => {
-        moviesFiltersManager.setMany(form.watch() as Partial<Record<string, unknown>>)
-    }, [form.watch()])
+        moviesFiltersManager.setMany(
+            debouncedValues as Partial<Record<string, unknown>>
+        );
+    }, [debouncedValues]);
 
-    const {data, ...props} = useGetAllMoviesRequest(form.watch())
+    const {data, ...props} = useGetAllMoviesRequest(debouncedValues);
 
-    return {data, form, ...props}
-}
+    return {data, form, ...props};
+};
 
-export {useGetAllMoviesPresenter}
+export {useGetAllMoviesPresenter};
