b7742f902806105266aaa47cb857e58d3a216caf
zelld <zeLLa444@gmail.com>
2026-01-28 12:32:32 +0700

    add color scheme light

--- src/app/module/movies/case/list/case/presenter.ts
+++ src/app/module/movies/case/list/case/presenter.ts
@@ -1,6 +1,6 @@
 import type {IGetAllMoviesForm} from "@/domain/movies/interface";
 import {useGetAllMoviesRequest} from "@/app/module/movies/case/list/case/request.ts";
-import {useForm} from "react-hook-form";
+import {useForm, useWatch} from "react-hook-form";
 import {useEffect} from "react";
 import {MoviesFiltersManager} from "@/app/utils/filters-manager.ts";
 import {useDebounce} from "@/app/utils/use-debounce.ts";
@@ -14,7 +14,9 @@ const useGetAllMoviesPresenter = () => {
         defaultValues,
     });
 
-    const watchedValues = form.watch();
+    const watchedValues = useWatch({
+        control: form.control,
+    });
 
     const debouncedValues = useDebounce(watchedValues, 500);
 
