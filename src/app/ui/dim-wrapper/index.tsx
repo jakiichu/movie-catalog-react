8e41220f266e887016cead0a379433b9dd83223d
jakiichu <zeLLda444@gmail.com>
2026-02-09 17:01:19 +0700

    edit bad comment

--- src/app/ui/dim-wrapper/index.tsx
+++ src/app/ui/dim-wrapper/index.tsx
@@ -3,9 +3,7 @@ import style from "./index.module.scss"
 import {cn} from "@/app/utils/cn.ts";
 import type {IDimWrapperProps} from "./interface.ts";
 
-// тут просто как блок если вы сейчас сидите в Ide от jetbrains 2025.3.1 вокруг вас будут такие же блоки
-
-// чтоб не писать свичкейсы можно вот такую красоту написать и получится рабочий вариант
+// чтоб не писать switch-case можно вот такую красоту написать и получится рабочий вариант
 const colorVariants = {
     main: style.mainColor,
     additional: style.additionalColor
